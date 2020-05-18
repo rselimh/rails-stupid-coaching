f584325bf082d86c148fd1a45df5b34a1e733458
Robert Hill <hill.rws@gmail.com>
2020-05-18 14:32:05 +0200

    creates form in ask.html.erb, which will take the user input and save it toparams[:question] (because name='question'). creates get answer and answer action route to routes.rb. Adds ruby logic to calculate value of @answer instance variable in the answer method in Questions Controller. Displays question and answer in answer.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+
+  # root to: "pages#home"
+  # GET /about
+  # get 'about', to: 'pages#about', as: :about
+  # verb path         controller#action  prefix
 end
